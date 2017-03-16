1ec0ed94a667e3f521ae0b7eeeb1b5f486c8cb09
gzhangzy <gzhangzy@gmail.com>
2017-03-16 17:04:24 +0800

    Update pdns.sql

--- pdns.sql
+++ pdns.sql
@@ -29,13 +29,89 @@ CREATE TABLE `records` (
     ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
+DROP FUNCTION IF EXISTS soasn;
+DELIMITER $$
+CREATE FUNCTION soasn(x CHAR(255))
+RETURNS CHAR(255) DETERMINISTIC
+BEGIN
+    DECLARE len INT;
+    DECLARE str CHAR(255);
+    DECLARE llen INT;
+    DECLARE lstr CHAR(255);
+    DECLARE rlen INT;
+    DECLARE rstr CHAR(255);
+    DECLARE sn0 INT;
+    DECLARE sn1 INT;
+    SET len = LENGTH(x);
+    SET llen = LENGTH(SUBSTRING_INDEX(x,' ',2));
+    SET lstr = LEFT(x, llen);
+    SET rlen = len-1-LENGTH(SUBSTRING_INDEX(x,' ',3));
+    SET rstr = RIGHT(x, rlen);
+    SET str = SUBSTRING(x,llen+2,len-2-llen-rlen);
+    SET sn0 = CONVERT(str, UNSIGNED)+1;
+    SET sn1 = CURDATE()*100;
+    IF sn1 < sn0 THEN
+        SET sn1 = sn0;
+    END IF;
+    RETURN CONCAT(lstr,' ',sn1,' ',rstr);
+END$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS after_insert_domains;
 DELIMITER $$
 CREATE TRIGGER after_insert_domains
 AFTER INSERT ON domains
 FOR EACH ROW
 BEGIN
-    insert into records values(0,new.id,new.name,'SOA','ns.zhuoyue.com root@zhuoyue.com 0 28800 7200 604800 3600',3600,0,UNIX_TIMESTAMP());
     insert into records values(0,new.id,new.name,'NS','ns.zhuoyue.com',3600,0,UNIX_TIMESTAMP());
     insert into records values(0,new.id,new.name,'NS','ns2.zhuoyue.com',3600,0,UNIX_TIMESTAMP());
+    insert into records values(0,new.id,new.name,'SOA','ns.zhuoyue.com root@zhuoyue.com 0 28800 7200 604800 3600',3600,0,UNIX_TIMESTAMP());
+END $$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS before_insert_records;
+DELIMITER $$
+CREATE TRIGGER before_insert_records
+BEFORE INSERT ON records
+FOR EACH ROW
+BEGIN
+    DECLARE old_domain_id int(11);
+    IF new.type='SOA' OR new.type='NS' THEN
+        select domain_id into old_domain_id from records where domain_id=new.domain_id and type='SOA';
+        IF old_domain_id THEN
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'deny insert SOA|NS record';
+        END IF;
+    END IF;
+END $$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS before_update_records;
+DELIMITER $$
+CREATE TRIGGER before_update_records
+BEFORE UPDATE ON records
+FOR EACH ROW
+BEGIN
+    IF old.type='SOA' OR old.type='NS' THEN
+        IF old.domain_id!=new.domain_id OR old.name!=new.name OR old.type!=new.type OR
+           old.type='NS' AND old.content!=new.content THEN
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'deny update SOA|NS record';
+        END IF;
+    END IF;
+END $$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS before_delete_records;
+DELIMITER $$
+CREATE TRIGGER before_delete_records
+BEFORE DELETE ON records
+FOR EACH ROW
+BEGIN
+    DECLARE old_id int(11);
+    IF old.type='SOA' OR old.type='NS' THEN
+        select id into old_id from domains where name=old.name;
+        IF old_id THEN
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'deny delete SOA|NS record';
+        END IF;
+    END IF;
 END $$
 DELIMITER ;
